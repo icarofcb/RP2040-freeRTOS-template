4888b2c0303e1190a7bab2d60acc3cecb2c61eaa
icarofcb <icarofcb@Outlook.com>
2024-10-14 14:26:42 -0300

    added debug feature

--- src/main.c
+++ src/main.c
@@ -3,26 +3,31 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+void led_task(void *arg);
 
-void led_task()
-{   
+int main(){
+    stdio_init_all();
+
+    printf("Programa Blink-LED FreeRTOS \n");
+
+    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    vTaskStartScheduler();
+
+    while(1){
+        vTaskDelay(pdMS_TO_TICKS(1));
+    };
+}
+
+void led_task(void *arg){   
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
+        printf("LIGA LED\n");
         vTaskDelay(pdMS_TO_TICKS(200));
         gpio_put(LED_PIN, 0);
+        printf("DESLIGA LED\n");
         vTaskDelay(pdMS_TO_TICKS(200));
     }
-}
-
-int main()
-{
-    stdio_init_all();
-
-    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
-    vTaskStartScheduler();
-
-    while(1){};
 }
